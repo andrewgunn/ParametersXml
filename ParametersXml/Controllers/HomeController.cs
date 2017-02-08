64a4ace7f1176aa3cf68d6a430709edcfde6c283
andrewgunn <hello@andrewgunn.co.uk>
2017-02-08 12:05:47 +0000

    Do all the things

---
+++ ParametersXml/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace ParametersXml.Controllers
+{
+   using System;
+   using System.Web.Mvc;
+
+   public class HomeController : Controller
+   {
+      [HttpGet]
+      public ActionResult Index()
+      {
+         throw new Exception();
+      }
+   }
+}
